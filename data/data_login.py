adb02a246a3c2fbb59bd1560b8e1cf6395625664
mariasmarchi <meryeclipse88@gmail.com>
2025-12-14 21:40:25 -0300

    actualizacion de CASOS LOGIN

--- data/data_login.py
+++ data/data_login.py
@@ -2,4 +2,5 @@ CASOS_LOGIN = [
     ("standard_user", "secret_sauce", True),     # usuario válido, login exitoso
     ("locked_out_user", "secret_sauce", False),  # usuario bloqueado, login falla
     ("usuario_malo", "password_malo", False),    # credenciales inválidas, login falla
+    ("error_user", "secret_sauce", False)       # usuario con error, login falla
 ]
