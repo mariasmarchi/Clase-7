7e6e07a83ba63d366e50e8b60f12ff120b77b0fd
mariasmarchi <meryeclipse88@gmail.com>
2025-12-15 21:07:58 -0300

    agregado de test2

--- test/login_invalido.py
+++ test/login_invalido.py
@@ -1,15 +1,10 @@
 import pytest
-from pages.cart_page import CartPage
-from pages.checkout_page import CheckoutPage
+from pages.login_page import LoginPage
 
 @pytest.mark.usefixtures("driver")
-def test_checkout(driver):
-    cart = CartPage(driver)
-    cart.ir_al_carrito()
-    cart.ir_a_checkout()
+def test_login_invalido(driver):
+    login = LoginPage(driver)
+    login.ir_a_login()
+    login.login("usuario_falso", "clave_incorrecta")
 
-    checkout = CheckoutPage(driver)
-    checkout.completar_datos_envio("Maria", "Marchi", "Buenos Aires")
-    checkout.confirmar_compra()
-
-    assert checkout.verificar_confirmacion(), "La compra no se completó correctamente"
+    assert login.mensaje_error_visible(), "No apareció mensaje de error en login inválido"
---
+++ test/test_checkoutProd_Neg.py
@@ -0,0 +1,10 @@
+import pytest
+from pages.checkout_page import CheckoutPage
+
+#Checkout sin productos (negativo extra)
+@pytest.mark.usefixtures("driver")
+def test_checkout_sin_productos(driver):
+    checkout = CheckoutPage(driver)
+    checkout.ir_a_checkout()
+
+    assert checkout.mensaje_carrito_vacio(), "Se permitió checkout sin productos en el carrito"
